899a50ea4c3ed05d1013971654700e52456a0832
vetero4ok <vetero4ok@gmail.com>
2021-08-09 12:18:50 +0300

    homework les 3, practice for API and maping object

--- src/lessons/lesson3/Lesson3.tsx
+++ src/lessons/lesson3/Lesson3.tsx
@@ -4,9 +4,9 @@ import './lesson_3';
 
 const Lesson3 = () => {
     const [searchName, setSearchName] = useState('');
-    const [searchResult, setSearchResult] = useState('');
+    const [searchResult, setSearchResult] = useState<Array<any>>([]);
     const [searchNameByType, setSearchNameByType] = useState('');
-    const [searchResultByType, setSearchResultByType] = useState('');
+    const [searchResultByType, setSearchResultByType] = useState<Array<any>>([]);
 
     const searchFilm = () => {
         API.searchFilmsByTitle(searchName)
@@ -14,14 +14,27 @@ const Lesson3 = () => {
                 console.log(data);
                 const {Response, Search, Error} = data;
                 if (Response === 'True') {
-                    setSearchResult(JSON.stringify(Search));
+                    //setSearchResult(JSON.stringify(Search));
+                    setSearchResult(Search);
 
                 } else
-                    setSearchResult(Error);
+                    setSearchResult([Error]);
             })
 
     };
-
+    // const searchFilm = async () => {
+    //     try {
+    //         const { data } = await API.searchFilmsByTitle(searchName);
+    //         const { Response, Search, Error } = data;
+    //         if (Response === 'True') {
+    //             setSearchResult(JSON.stringify(Search));
+    //         } else {
+    //             setSearchResult(Error);
+    //         }
+    //     } catch (e) {
+    //         console.log(`some error exist `, e);
+    //     }
+    // }
     const searchByType = (e: React.MouseEvent<HTMLButtonElement>) => {
         const type: string = e.currentTarget.dataset.t ? e.currentTarget.dataset.t : '';
         API.searchFilmsByType(searchNameByType, type)
@@ -29,7 +42,8 @@ const Lesson3 = () => {
                 console.log(data);
                 const {Search, Response, Error} = data;
                 if (Response === 'True') {
-                    setSearchResultByType(JSON.stringify(Search))
+                   // setSearchResultByType(JSON.stringify(Search))
+                   setSearchResultByType(Search)
                 } else setSearchResultByType(Error)
             })
 
@@ -43,12 +57,7 @@ const Lesson3 = () => {
                 <h3><p>Search by name:</p></h3>
                 <input type="text" value={searchName} onChange={(e) => setSearchName(e.currentTarget.value)}/>
                 <button onClick={searchFilm}>Search</button>
-                <div>
-                    {
-                        searchResult
 
-                    }
-                </div>
             </div>
 
             <div>
@@ -57,8 +66,36 @@ const Lesson3 = () => {
                        onChange={(e) => setSearchNameByType(e.currentTarget.value)}/>
                 <button onClick={searchByType} data-t="movie">Movie</button>
                 <button onClick={searchByType} data-t="series">Series</button>
+                <br/>
                 <div>
-                    {searchResultByType}
+                    {
+                        searchResult.map((ser, index) => {
+                            return (
+                                <span key={index}>
+                                <img src={ser?.Poster} alt={'poster'}/>
+                                <h4>{ser?.Title}</h4>
+                                <p>{ser?.Type}</p>
+                                <p>{ser?.Year}</p>
+                                <p>{ser?.imdbID}</p>
+                                </span>);
+                        })
+
+                    }
+                </div>
+                <div>
+                    {
+                        searchResultByType.map((ser, index) => {
+                            return (
+                                <span key={index}>
+                                <img src={ser?.Poster} alt={'poster'}/>
+                                <h4>{ser?.Title}</h4>
+                                <p>{ser?.Type}</p>
+                                <p>{ser?.Year}</p>
+                                <p>{ser?.imdbID}</p>
+                                </span>);
+                        })
+
+                    }
                 </div>
             </div>
         </div>
